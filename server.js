60aff9b77c64f3aee77a7009a05a4629a3c332fa
Chris-de-sypher <christiansilvester700@gmail.com>
2023-05-09 12:36:04 +0100

    Done

--- server.js
+++ server.js
@@ -2,24 +2,39 @@
 
 const express = require("express");
 const { resolve } = require("path");
-
+const PORT = process.env.PORT || 4000;
 const app = express();
+const dotenv = require("dotenv");
+const serverless = require("serverless-http");
+const router = express.Router();
+const hostname = '0.0.0.0';
 
-app.use(express.static("./style"));
-app.use(express.static("./images"));
-app.use(express.static("./javascript"));
-app.use(express.static("./userImg"));
-app.use(express.static("./public"));
+app.use(express.static(resolve(__dirname, "./src/images")));
+app.use(express.static(resolve(__dirname, "./src/style")));
+app.use(express.static(resolve(__dirname, "./src/javascript")));
+app.use(express.static(resolve(__dirname, "./src/userImg")));
+app.use(express.static(resolve(__dirname, "./src/Html")));
 
-app.get("/", (req, res) => {
-  res.status(200).sendFile(resolve(__dirname, "./index.html"));
+router.get("/", (req, res) => {
+  res.status(200).sendFile(resolve(__dirname, "./src/Html/index.html"));
 });
 
-app.all('*', (req, res) => {
-    res.status(404).sendFile(resolve(__dirname, './error.html'))
-})
+router.get("./portfolio", (req, res) => {
+  if (req.url == 'portfolio.html') {
+      res.status(200).sendFile(resolve(__dirname, "./src/Html/portfolio.html"));
+  }
+});
 
+router.all("*", (req, res) => {
+  res.status(404).sendFile(resolve(__dirname, "./src/Html/error.html"));
+});
 
-app.listen(4000, () => {
+app.use(router);
+
+app.listen(PORT, hostname, () => {
   console.log("user hit the resource");
 });
+
+
+
+// module.exports.handler = serverless(app);
